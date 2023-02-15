ba7de52ff70958fbc01a9597c964826bd591b517
jt2404 <jayhthakkar2404@gmail.com>
2023-02-15 07:28:24 +0530

    changes for pg details page

--- client/src/components/PgDetails/index.js
+++ client/src/components/PgDetails/index.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./index.css";
 import axios from "axios";
+import Headerbar from "../Header.js";
+const { Component } = React;
 const PgDetails = () => {
   let { id } = useParams();
   const [pgInfo, setPgInfo] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
       try {
         const pgdata = await axios.get(`http://localhost:8000/pg/${id}`, {
           headers: {
             authorization: JSON.parse(localStorage.getItem("token")),
           },
         });
-        console.log(pgdata);
-        setPgInfo(pgdata?.data);
-        setLoading(false);
+        if (pgdata?.status === 200) {
+          setPgInfo(pgdata?.data);
+          setLoading(false);
+        }
       } catch (error) {
         if (error?.response?.data?.result === "Please provide a valid token") {
           localStorage.removeItem("user");
@@ -27,9 +30,73 @@ const PgDetails = () => {
     };
     fetchData();
   }, [id]);
+  console.log(pgInfo?.images);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
-      <p>Pg details name <h1> {pgInfo.name}</h1></p>
+    <>
+      <div class="card-wrapper">
+        <div class="card-for-details">
+          <div class="product-imgs">
+            <div class="img-display">
+              <div class="img-showcase">
+                <img src={pgInfo?.images[0]} alt="shoe image" />
+              </div>
+            </div>
+          </div>
+          <div className="product-content">
+            <div class="product-rating">
+              {/* <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+              <span>4.7(21)</span> */}
+            </div>
+            <div class="product-price">
+              {/* <p class="last-price">
+                Old Price: <span>$257.00</span>
+              </p>
+              <p class="new-price">
+                New Price: <span>$249.00 (5%)</span>
+              </p> */}
+            </div>
+            <div class="product-detail">
+              <h2> {pgInfo?.name}</h2>
+              {/* <p>
+              {pgInfo?.description}
+              </p> */}
+              
+              <ul>
+                <li>
+                  Address: <span>{pgInfo?.address}</span>
+                </li>
+                <li>
+                  Available: <span>in stock</span>
+                </li>
+                <li>
+                  Category: <span>{pgInfo?.roomtype}</span>
+                </li>
+                <li>
+                  Number Of Rooms Available: <span>{pgInfo?.availableroom}</span>
+                </li>
+                <li>
+                  Price: <span>{pgInfo?.price}</span>
+                </li>
+                
+              </ul>
+              <p>
+              {pgInfo?.description}
+              </p>
+            </div>
+            
+            
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
